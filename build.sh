0baec2176e1dcccf0c53f53c6af504be1c34bc7d
michaelmartinez <martinezmj@uthscsa.edu>
2013-08-20 16:40:21 -0500

    Added build number increment.

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-cat classes/platform.js classes/gunzip.js classes/header.js classes/orientation.js classes/utilities.js classes/nifti.js classes/volume.js classes/imagedimensions.js classes/voxeldimensions.js classes/imagetype.js classes/imagedata.js classes/header-nifti.js classes/screenslice.js classes/coordinate.js classes/imagerange.js classes/viewer.js | java -jar yuicompressor.jar --type js -o papaya.js
+#!/bin/bash
+. ./build.properties
+PAPAYA_BUILD_NUM=$((PAPAYA_BUILD_NUM + 1))
+
+echo "PAPAYA_VERSION_ID="$PAPAYA_VERSION_ID > build.properties
+echo "PAPAYA_BUILD_NUM="$PAPAYA_BUILD_NUM >> build.properties
+
+cat build.properties classes/platform.js classes/gunzip.js classes/header.js classes/orientation.js classes/utilities.js classes/nifti.js classes/volume.js classes/imagedimensions.js classes/voxeldimensions.js classes/imagetype.js classes/imagedata.js classes/header-nifti.js classes/screenslice.js classes/coordinate.js classes/imagerange.js classes/viewer.js | java -jar yuicompressor.jar --type js -o papaya.js
 
 
 
