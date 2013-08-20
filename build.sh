56c94df60bf244b8a0fd3c9483285ddd79ed31f6
michaelmartinez <martinezmj@uthscsa.edu>
2013-08-20 14:08:39 -0500

    Fixed order of js files.

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-cat classes/*.js | java -jar yuicompressor.jar --type js -o papaya.js
+cat classes/platform.js classes/gunzip.js classes/header.js classes/orientation.js classes/utilities.js classes/nifti.js classes/volume.js classes/imagedimensions.js classes/voxeldimensions.js classes/imagetype.js classes/imagedata.js classes/header-nifti.js classes/viewer.js classes/screenslice.js classes/coordinate.js classes/imagerange.js | java -jar yuicompressor.jar --type js -o papaya.js
+
+
+
