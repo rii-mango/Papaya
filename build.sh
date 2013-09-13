875ea9bff228a1903121e775a62c3c3223a19866
michaelmartinez <martinezmj@uthscsa.edu>
2013-09-13 09:43:12 -0500

    More refactoring and cleanup.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 . ./build.properties
 PAPAYA_BUILD_NUM=$((PAPAYA_BUILD_NUM + 1))
@@ -8,6 +9,8 @@ echo "PAPAYA_BUILD_NUM="$PAPAYA_BUILD_NUM >> build.properties
 rm -rf build
 mkdir build
 
-cat build.properties classes/constants.js classes/data/*.js classes/core/*.js classes/utilities/*.js classes/volume/*.js classes/volume/nifti/*.js classes/viewer/*.js classes/main.js  | java -jar lib/yuicompressor.jar --type js -o papaya.js
+cat build.properties jquery/jquery.js jquery/jquery-ui.custom.js classes/constants.js classes/data/*.js classes/core/*.js classes/utilities/*.js classes/volume/*.js classes/volume/nifti/*.js classes/viewer/*.js classes/main.js  | java -jar lib/yuicompressor.jar --type js -o build/papaya.js
+cat css/ui-darkness/jquery-ui.custom.css css/main.css | java -jar lib/yuicompressor.jar --type css -o build/papaya.css
+cp examples/index.html build/.
 
-cat css/ui-darkness/jquery-ui.custom.css css/main.css | java -jar yuicompressor.jar --type css -o papaya.css
+echo "Done!"
