10788f314c6a6837ee5a645f70eb45d029d05640
michaelmartinez <martinezmj@uthscsa.edu>
2013-09-19 16:35:19 -0500

    Fixed the build script.

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ echo "PAPAYA_BUILD_NUM="$PAPAYA_BUILD_NUM >> build.properties
 rm -rf build
 mkdir build
 
-cat build.properties jquery/jquery.js jquery/jquery-ui.custom.js classes/constants.js classes/data/*.js classes/core/*.js classes/utilities/*.js classes/volume/*.js classes/volume/nifti/*.js classes/viewer/*.js classes/main.js  | java -jar lib/yuicompressor.jar --type js -o build/papaya.js
-cat css/ui-darkness/jquery-ui.custom.css css/main.css | java -jar lib/yuicompressor.jar --type css -o build/papaya.css
+cat build.properties jquery/jquery.js classes/constants.js classes/data/*.js classes/core/*.js classes/volume/*.js classes/volume/nifti/*.js classes/viewer/*.js classes/ui/*.js classes/main.js classes/utilities/*.js  | java -jar lib/yuicompressor.jar --type js -o build/papaya.js
+cat css/main.css | java -jar lib/yuicompressor.jar --type css -o build/papaya.css
 cp examples/index.html build/.
 
 echo "Done!"
