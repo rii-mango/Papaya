1101b055a64434406947782b8124096587868d80
michaelmartinez <martinezmj@uthscsa.edu>
2013-10-28 10:26:56 -0500

    Fixed some issues found when not including sample image and/or atlas when building.

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# increment build number
+. ./build.properties
+PAPAYA_BUILD_NUM=$((PAPAYA_BUILD_NUM + 1))
+echo "PAPAYA_VERSION_ID="$PAPAYA_VERSION_ID";" > build.properties
+echo "PAPAYA_BUILD_NUM="$PAPAYA_BUILD_NUM";" >> build.properties
+
 java -jar lib/papaya-builder.jar $*
