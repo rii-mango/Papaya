e0e6b454f9620c22cd5671e36d312c5b024eea4f
michaelmartinez <martinezmj@uthscsa.edu>
2015-10-28 09:00:29 -0500

    added java version check

--- papaya-builder.sh
+++ papaya-builder.sh
@@ -7,6 +7,13 @@ then
     rm -r build
 fi
 
+JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+if [[ "$JAVA_VER" < "17" ]]; then
+    echo "Java 7 or higher is required!  Your version is..."
+    java -version
+    exit 1
+fi
+
 java -Xmx512M -jar lib/papaya-builder.jar $*
 
 if [ -f build/papaya.js ]
